8bdd136a8e57b941f388431f0f397b8014ff52eb
Kim <isyikuan@foxmail.com>
2019-11-06 14:23:52 +0800

    Modify TimeUtils.

--- library/src/main/java/com/yikuan/androidcommon/util/TimeUtils.java
+++ library/src/main/java/com/yikuan/androidcommon/util/TimeUtils.java
@@ -14,29 +14,13 @@ import java.util.Map;
  * @date 2019/10/04
  */
 public class TimeUtils {
-    private static final Map<String, ThreadLocal<SimpleDateFormat>> SDF_MAP = new HashMap<>();
+    private static final Map<String, ThreadLocal<SimpleDateFormat>> sMap = new HashMap<>();
+
+    private static final String PATTERN_DEFAULT = "yyyy-MM-dd HH:mm:ss";
+    private static final String PATTERN_FILE_NAME = "yyyyMMddHHmmsss";
 
     private TimeUtils() {
         throw new UnsupportedOperationException("cannot be instantiated.");
-    }
-
-    private static SimpleDateFormat getSdf(final String pattern) {
-        if (pattern == null) {
-            throw new NullPointerException("pattern cannot be null.");
-        }
-        ThreadLocal<SimpleDateFormat> threadLocal = SDF_MAP.get(pattern);
-        if (threadLocal == null) {
-            synchronized (TimeUtils.class) {
-                threadLocal = SDF_MAP.get(pattern);
-                if (threadLocal == null) {
-                    threadLocal = new ThreadLocal<>();
-                    threadLocal.set(new SimpleDateFormat(pattern, Locale.getDefault()));
-                    SDF_MAP.put(pattern, threadLocal);
-                }
-            }
-        }
-
-        return threadLocal.get();
     }
 
     public static String format(Date date, String pattern) {
@@ -46,10 +30,6 @@ public class TimeUtils {
     public static Date parse(String dateStr, String pattern) throws ParseException {
         return getSdf(pattern).parse(dateStr);
     }
-
-
-    private static final String PATTERN_DEFAULT = "yyyy-MM-dd HH:mm:ss";
-    private static final String PATTERN_FILE_NAME = "yyyyMMddHHmmsss";
 
     public static String millis2String(long millis) {
         return millis2String(millis, getSdf(PATTERN_DEFAULT));
@@ -64,6 +44,25 @@ public class TimeUtils {
 
     public static String formatFileName() {
         return getSdf(PATTERN_FILE_NAME).format(new Date());
+    }
+
+    private static SimpleDateFormat getSdf(final String pattern) {
+        if (pattern == null) {
+            throw new NullPointerException("pattern cannot be null.");
+        }
+        ThreadLocal<SimpleDateFormat> threadLocal = sMap.get(pattern);
+        if (threadLocal == null) {
+            synchronized (TimeUtils.class) {
+                threadLocal = sMap.get(pattern);
+                if (threadLocal == null) {
+                    threadLocal = new ThreadLocal<>();
+                    threadLocal.set(new SimpleDateFormat(pattern, Locale.getDefault()));
+                    sMap.put(pattern, threadLocal);
+                }
+            }
+        }
+
+        return threadLocal.get();
     }
 
 }

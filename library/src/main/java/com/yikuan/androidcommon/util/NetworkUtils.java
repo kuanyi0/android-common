ba35d9a2200aa375a01f3d84476174b1e35d3aeb
Kim <isyikuan@foxmail.com>
2020-09-23 11:40:29 +0800

    Modify NetworkUtils

--- library/src/main/java/com/yikuan/androidcommon/util/NetworkUtils.java
+++ library/src/main/java/com/yikuan/androidcommon/util/NetworkUtils.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+import androidx.annotation.RequiresPermission;
+
 import com.yikuan.androidcommon.AndroidCommon;
+
+import static android.Manifest.permission.ACCESS_NETWORK_STATE;
 
 /**
  * @author yikuan
@@ -22,21 +26,25 @@ public class NetworkUtils {
         throw new UnsupportedOperationException("cannot be instantiated.");
     }
 
+    @RequiresPermission(ACCESS_NETWORK_STATE)
     public static boolean isConnected() {
         NetworkInfo networkInfo = sConnectivityManager.getActiveNetworkInfo();
         return networkInfo != null && networkInfo.isAvailable() && networkInfo.isConnected();
     }
 
+    @RequiresPermission(ACCESS_NETWORK_STATE)
     public static boolean isWifiConnected() {
         NetworkInfo networkInfo = sConnectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
         return networkInfo != null && networkInfo.isAvailable() && networkInfo.isConnected();
     }
 
+    @RequiresPermission(ACCESS_NETWORK_STATE)
     public static boolean isMobileConnected() {
         NetworkInfo networkInfo = sConnectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
         return networkInfo != null && networkInfo.isAvailable() && networkInfo.isConnected();
     }
 
+    @RequiresPermission(ACCESS_NETWORK_STATE)
     public static int getConnectedType() {
         NetworkInfo networkInfo = sConnectivityManager.getActiveNetworkInfo();
         return networkInfo == null ? -1 : networkInfo.getType();

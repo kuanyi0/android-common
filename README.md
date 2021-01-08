689d4d133611b2d5ed07f8ff35e97d0428d9e7ae
Kim <isyikuan@foxmail.com>
2021-01-08 19:11:42 +0800

    Modify LogUtils and update README.md

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Gradle:
 
 ```
-implementation 'com.kimyi:android-common:0.2.8'
+implementation 'com.kimyi:android-common:0.2.9'
 ```
 
 
@@ -25,3 +25,12 @@ class App : Application() {
 }
 ```
 
+## Feature
+
+- base
+	- mvp: BaseView, BasePresenter
+	- mvp2: BaseActivity, BaseFragment, BasePresenter
+	- observe: BaseObservable, BaseObserver, BaseObserveManager
+	- widget: BaseDialog, BaseLayout, BaseBindingLayout, BaseLifecycleBindingLayout, BaseComponentLayout
+- ui: CustomDialog, MovableLayout...
+- util: BitmapUtils, CrashHandler, DateUtils...
--- library/src/main/java/com/yikuan/androidcommon/util/LogUtils.java
+++ library/src/main/java/com/yikuan/androidcommon/util/LogUtils.java
@@ -36,6 +36,7 @@ public class LogUtils {
 
     @Level
     private static int sLevel = VERBOSE;
+    private static boolean sSave = true;
 
     private LogUtils() {
         throw new UnsupportedOperationException("cannot be instantiated");
@@ -45,39 +46,55 @@ public class LogUtils {
         sLevel = level;
     }
 
+    public static void setSave(boolean save) {
+        sSave = save;
+    }
+
     public static void v(String tag, String msg) {
         if (sLevel <= VERBOSE) {
             Log.v(tag, msg);
+            checkAndSave(tag, msg);
         }
     }
 
     public static void d(String tag, String msg) {
         if (sLevel <= DEBUG) {
             Log.d(tag, msg);
+            checkAndSave(tag, msg);
         }
     }
 
     public static void i(String tag, String msg) {
         if (sLevel <= INFO) {
             Log.i(tag, msg);
+            checkAndSave(tag, msg);
         }
     }
 
     public static void w(String tag, String msg) {
         if (sLevel <= WARN) {
             Log.w(tag, msg);
+            checkAndSave(tag, msg);
         }
     }
 
     public static void e(String tag, String msg) {
         if (sLevel <= ERROR) {
             Log.e(tag, msg);
+            checkAndSave(tag, msg);
         }
     }
 
     public static void wtf(String tag, String msg) {
         if (sLevel <= ASSERT) {
             Log.wtf(tag, msg);
+            checkAndSave(tag, msg);
+        }
+    }
+
+    private static void checkAndSave(String tag, String msg) {
+        if (sSave) {
+            write(tag, msg);
         }
     }
 
